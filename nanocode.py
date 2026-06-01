853260bba3ace6dc89f92b8916cc0634bb0e7a70
qwerlty123 <1787129184@qq.com>
2026-06-01 04:16:45 -0700

    Bump version to 0.5.9

--- nanocode.py
+++ nanocode.py
@@ -55,7 +55,7 @@ from prompt_toolkit.widgets import SearchToolbar
 from rich.console import Console
 from rich.markdown import Markdown
 
-__version__ = "0.5.8"
+__version__ = "0.5.9"
 
 Json = dict[str, Any]
 HTTP_USER_AGENT = "nanocode/" + __version__
