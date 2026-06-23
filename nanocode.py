cdf874278027773758a146ebb40b08329d0103ef
qwerlty123 <1787129184@qq.com>
2026-06-23 06:35:47 -0700

    bump 0.6.6

--- nanocode.py
+++ nanocode.py
@@ -59,7 +59,7 @@ from rich.console import Console
 from rich.markdown import Markdown
 from rich.rule import Rule
 
-__version__ = "0.6.5"
+__version__ = "0.6.6"
 
 Json = dict[str, Any]
 HTTP_USER_AGENT = "nanocode/" + __version__
