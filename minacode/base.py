853a75f7c8db7a94c3074d489057f945c9f23a52
qwerlty123 <1787129184@qq.com>
2026-08-02 05:34:35 -0700

    Release 0.19.0

--- minacode/base.py
+++ minacode/base.py
@@ -30,7 +30,7 @@ except ImportError:  # pragma: no cover - optional highlighting dependency
     pygments = None
     Token = None  # keep the name defined so class-body/token lookups don't NameError
 
-__version__ = "0.18.1"
+__version__ = "0.19.0"
 
 _ResourceT = TypeVar("_ResourceT")
 
